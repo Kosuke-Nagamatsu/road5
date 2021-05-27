31ca54e9e9d724f9a57a8ae07a0b60ed1780a9be
Kosuke-Nagamatsu <hateruma39@gmail.com>
2021-05-27 22:36:21 +0900

    corrected: 3行目と10行目

--- web_tech/test.rb
+++ web_tech/test.rb
@@ -1,13 +1,13 @@
 require 'webrick'
 server = WEBrick::HTTPServer.new({
-  :DocumentRoot => '/',
+  :DocumentRoot => '',
   :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
   :Port => '3000',
 })
 ['INT', 'TERM'].each {|signal|
   Signal.trap(signal){ server.shutdown }
 }
-server.mount('/task', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
+server.mount('/', WEBrick::HTTPServlet::ERBHandler, 'task.html.erb')
 server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
 # <form action='goya2.cgi'> 〜 </form>の内部にある値を、goya2.rbに送信できるようにする
 server.mount('/goya2.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya2.rb')
